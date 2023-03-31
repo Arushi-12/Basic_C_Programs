77ff2081ae815906c78c6a062a0f5a32cd1f6771
Arushi-12 <118588760+Arushi-12@users.noreply.github.com>
2023-04-01 00:49:26 +0530

    Day_1_Project

---
+++ Area_of_Circle.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int radius;
+    printf("Enter the radius: ");
+    scanf("%d",&radius);
+    printf("The area of the circle is: %d",radius * radius * 3.14);
+    return 0;
+}
