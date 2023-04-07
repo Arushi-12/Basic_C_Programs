df2516310962a72128c89899d947040094118a27
Arushi-12 <118588760+Arushi-12@users.noreply.github.com>
2023-04-07 14:57:47 +0530

    Day_4

---
+++ Grading_Program.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int marks;
+    printf("Enter the marks");
+    scanf("%d",&marks);
+    if (marks >= 0 && marks < 30){
+        printf("The grade is: C");
+    } else if (marks >= 30 && marks < 70){
+        printf("The grade is: B");
+    } else if (marks >= 70 && marks < 90 ){
+        printf("The grade is: A");
+    } else if (marks >= 90 && marks <= 100){
+        printf("The grade is: A+");
+    } else{
+        printf("Invalid marks!");
+    }
+}
